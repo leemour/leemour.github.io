e142ec8b0a328e9398661734737dd4bada24d612
Viacheslav Ptsarev <leemour@gmail.com>
2016-05-24 03:40:50 +0300

    Configure Jekyll. Install Agency theme

---
+++ js/main.js
@@ -0,0 +1,14 @@
+---
+layout: nil
+---
+
+/* Add your includes. */
+
+{% include js/jquery.js %}
+{% include js/bootstrap.min.js %}
+{% include js/classie.js %}
+{% include js/jquery.easing.min.js %}
+{% include js/cbpAnimatedHeader.min.js %}
+{% include js/contact_me.js %}
+{% include js/jqBootstrapValidation.js %}
+{% include js/agency.js %}
